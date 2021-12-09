4b9d142fcbc992f3ed67f2cf7118c04c4805e4b0
mdimranh <imran15-1984@diu.edu.bd>
2021-12-09 12:39:30 +0600

    Add post method in videoformsubmit

--- home/views.py
+++ home/views.py
@@ -150,6 +150,8 @@ def Folder(request,channel_name, name, id):
 @csrf_exempt
 def Upload_video_save(request, channel_name, id):
     if request.method == 'POST':
+        print("im am here-------------------------------")
+        print(request.POST['title'])
         if 'title' in request.POST:
             print("im am here-------------------------------")
             Channel = channel.objects.get(id = id)
